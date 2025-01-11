fc7f6fedf21c6a31bb2f33d2ea7f15ca501e2b2c
edison <845292122@qq.com>
2025-01-11 15:17:26 +0800

    feat: 整合helmet

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
 import { csrfProtection, corsOptions } from './config'
 
 const app = express()
 
 /**
  * * 基础中间件
+ * helmet: 安全中间件
  * cors: 跨域中间件
  * express.json(): 解析请求体中间件
  * cookieParser(): 解析cookie中间件
  * csrfProtection: csrf中间件
  */
+app.use(helmet())
 app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
