c22797bec550d23363b187e3a31144bfe49554f7
edison <845292122@qq.com>
2025-01-11 22:07:01 +0800

    style: 修改业务异常名称

--- src/common/error.ts
+++ src/common/error.ts
@@ -1,9 +1,9 @@
-export class BusinessError extends Error {
+export class BizError extends Error {
   public statusCode: number
 
   constructor(message: string, statusCode: number = 400) {
     super(message) // 调用父类的构造函数
-    this.name = 'BusinessError' // 错误名称
+    this.name = 'BizError' // 错误名称
     this.statusCode = statusCode // HTTP 状态码
   }
 }
