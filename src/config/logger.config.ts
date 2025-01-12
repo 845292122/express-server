a85ef6306b31498037e68973c83e62b00ae099e2
edison <845292122@qq.com>
2025-01-12 09:43:07 +0800

    style: 修改日志格式

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -8,15 +8,18 @@ import logger from '../helper/logger.helper'
 // }
 
 // 定义 morgan 中间件配置
-export const morganLogger = morgan(':method :url :status :remote-addr - :response-time ms', {
-  stream: {
-    write: message => {
-      const status = parseInt(message.split(' ')[2], 10) // 从日志中提取状态码
-      if (status >= 500) {
-        logger.error(`HTTP Error: ${message.trim()}`) // 错误级别日志
-      } else {
-        logger.info(message.trim()) // 普通信息日志
+export const morganLogger = morgan(
+  '请求方式: :method 请求url: :url 请求状态: :status 请求地址: :remote-addr - 响应时间: :response-time ms 总处理时间: :total-time ms',
+  {
+    stream: {
+      write: message => {
+        const status = parseInt(message.split(' ')[2], 10) // 从日志中提取状态码
+        if (status >= 500) {
+          logger.error(`HTTP Error: ${message.trim()}`) // 错误级别日志
+        } else {
+          logger.info(message.trim()) // 普通信息日志
+        }
       }
     }
   }
-})
+)
