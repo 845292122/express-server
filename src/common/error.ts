36c09845f186725d99acda6e4dd0a179072d9e1b
edison <845292122@qq.com>
2025-01-22 21:52:14 +0800

    style: 去除多余注释

--- src/common/error.ts
+++ src/common/error.ts
@@ -2,9 +2,9 @@ export class BizError extends Error {
   public code: number
 
   constructor(message: string, code: number = 400) {
-    super(message) // 调用父类的构造函数
-    this.name = 'BizError' // 错误名称
-    this.code = code // 业务状态码
+    super(message)
+    this.name = 'BizError'
+    this.code = code
 
     Object.setPrototypeOf(this, BizError.prototype)
   }
