7c899164e18b758b3e2e3bf36586a21cf4434cee
edison <845292122@qq.com>
2025-01-23 19:56:33 +0800

    feat: 修改权限

--- src/modules/perm/perm.handler.ts
+++ src/modules/perm/perm.handler.ts
@@ -22,5 +22,30 @@ export default {
     })
 
     httpOk(res)
+  },
+
+  modify: async (req: Request, res: Response) => {
+    const permInfo: InputPermType = req.body
+
+    const permExist = await PrismaHelper.perm.findFirst({
+      where: {
+        key: permInfo.key,
+        delFlag: 0,
+        id: {
+          not: permInfo.id
+        }
+      }
+    })
+
+    if (permExist) throw new BizError('该权限已存在')
+
+    await PrismaHelper.perm.update({
+      where: {
+        id: permInfo.id
+      },
+      data: permInfo
+    })
+
+    httpOk(res)
   }
 }
--- src/modules/perm/perm.router.ts
+++ src/modules/perm/perm.router.ts
@@ -8,5 +8,6 @@ const router = Router()
 
 // * 创建权限
 router.post('/create', rateLimiter(), validate(inputPermSchema), replyHelper(PermHandler.create))
-
+// * 修改权限
+router.post('/modify', rateLimiter(), validate(inputPermSchema), replyHelper(PermHandler.modify))
 export default router
