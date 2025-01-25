1efd324b0127a58eb31c3861a00a8dcdc03b0c84
edison <845292122@qq.com>
2025-01-25 17:00:08 +0800

    style: 修改错误返回格式

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -2,19 +2,8 @@ import { Request, Response, NextFunction } from 'express'
 import { BizError, NotFoundError } from '../common/error'
 import logger from '../helper/logger.helper'
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof BizError) {
-    res.status(200).json({
-      code: err.code,
-      message: err.message
-    })
-    return
-  }
-
-  if (err instanceof NotFoundError) {
-    res.status(200).json({
-      code: err.code,
-      message: err.message
-    })
+  if (err instanceof BizError || err instanceof NotFoundError) {
+    res.status(err.code).json({ message: err.message })
     return
   }
 
